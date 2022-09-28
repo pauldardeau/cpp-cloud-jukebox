84b4544da4026c7bb8b016622565521bf6ae8c23
Paul Dardeau <pauldardeau@gmail.com>
2022-09-27 19:53:22 -0500

    replace tabs with spaces

--- src/jukebox.cpp
+++ src/jukebox.cpp
@@ -1500,12 +1500,12 @@ void Jukebox::display_info() const {
       if (song_index + 3 <= max_index) {
          printf("----- songs on deck -----\n");
          const SongMetadata& first_song = song_list[song_index+1];
-	 printf("%s\n", first_song.fm.file_uid.c_str());
-	 const SongMetadata& second_song = song_list[song_index+2];
-	 printf("%s\n", second_song.fm.file_uid.c_str());
-	 const SongMetadata& third_song = song_list[song_index+3];
-	 printf("%s\n", third_song.fm.file_uid.c_str());
-	 printf("-------------------------\n");
+         printf("%s\n", first_song.fm.file_uid.c_str());
+         const SongMetadata& second_song = song_list[song_index+2];
+         printf("%s\n", second_song.fm.file_uid.c_str());
+         const SongMetadata& third_song = song_list[song_index+3];
+         printf("%s\n", third_song.fm.file_uid.c_str());
+         printf("-------------------------\n");
       }
    }
 }
