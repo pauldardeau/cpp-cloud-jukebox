0434769ce9abf21728bffb1e5b71e608503ded35
Paul Dardeau <pauldardeau@gmail.com>
2023-01-09 08:03:36 -0600

    add encoding helpers to jb_utils

--- src/jb_utils.cpp
+++ src/jb_utils.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+static const string DOUBLE_DASHES = "--";
+
 string JBUtils::unencode_value(const string& encoded_value) {
    string new_value = encoded_value;
    chaudiere::StrUtils::replaceAll(new_value, "-", " ");
@@ -15,3 +17,15 @@ string JBUtils::encode_value(const string& value) {
    return new_value;
 }
 
+string JBUtils::encode_artist_album(const string& artist,
+                                    const string& album) {
+   return encode_value(artist) + DOUBLE_DASHES +
+          encode_value(album);
+}
+
+string JBUtils::encode_artist_album_song(const string& artist,
+                                         const string& album,
+                                         const string& song) {
+   return encode_artist_album(artist, album) + DOUBLE_DASHES +
+          encode_value(song);
+}
--- src/jb_utils.h
+++ src/jb_utils.h
@@ -8,6 +8,11 @@ class JBUtils {
 public:
    static std::string unencode_value(const std::string& encoded_value);
    static std::string encode_value(const std::string& value);
+   static std::string encode_artist_album(const std::string& artist,
+                                          const std::string& album);
+   static std::string encode_artist_album_song(const std::string& artist,
+                                               const std::string& album,
+                                               const std::string& song);
 };
 
 #endif
